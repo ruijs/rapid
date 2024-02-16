d8897b3ac4ca6075080ff8d3a21ca7ed53266f25
Fossil <imzshh@gmail.com>
2024-02-16 11:10:19 +0800

    use rollup-plugin-tsc-alias

--- packages/rapid-core/rollup.config.js
+++ packages/rapid-core/rollup.config.js
@@ -1,22 +1,5 @@
-import { exec } from "child_process";
 import typescript from 'rollup-plugin-typescript2';
-
-const tscAlias = () => {
-    return {
-        name: "tsAlias",
-        writeBundle: () => {
-            return new Promise((resolve, reject) => {
-                exec("tsc-alias", function callback(error, stdout, stderr) {
-                    if (stderr || error) {
-                        reject(stderr || error);
-                    } else {
-                        resolve(stdout);
-                    }
-                });
-            });
-        },
-    };
-};
+import tscAlias from 'rollup-plugin-tsc-alias';
 
 export default {
     input: ["src/index.ts"],
