5eeac6b45140eb1c7098f24d0834d1d935318a56
Fossil <imzshh@gmail.com>
2024-07-11 00:04:34 +0800

    Added setSystemSettingValues action handler

--- packages/rapid-core/src/plugins/setting/routes/index.ts
+++ packages/rapid-core/src/plugins/setting/routes/index.ts
@@ -1,4 +1,5 @@
 import getUserSettingValues from "./getUserSettingValues";
 import getSystemSettingValues from "./getSystemSettingValues";
+import setSystemSettingValues from "./setSystemSettingValues";
 
-export default [getUserSettingValues, getSystemSettingValues];
+export default [getUserSettingValues, getSystemSettingValues, setSystemSettingValues];
