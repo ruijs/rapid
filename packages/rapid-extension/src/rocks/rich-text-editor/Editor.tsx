b45cfc094f53daf54d199124af2c784e234d9aaf
benyue.liu <benyue.liu@jimeng.io>
2024-07-23 11:13:31 +0800

    feat: 富文本可上传图片

--- packages/rapid-extension/src/rocks/rich-text-editor/Editor.tsx
+++ packages/rapid-extension/src/rocks/rich-text-editor/Editor.tsx
@@ -1,14 +1,16 @@
-import { useState, useEffect, memo } from "react";
+import { useState, useEffect, memo, useMemo } from "react";
 import { Editor, Toolbar } from "@wangeditor/editor-for-react";
-import { IDomEditor, IEditorConfig, IToolbarConfig } from "@wangeditor/editor";
+import { IDomEditor, IEditorConfig, IToolbarConfig, DomEditor } from "@wangeditor/editor";
+import { merge } from "lodash";
+import rapidApi from "../../rapidApi";
 
 import "@wangeditor/editor/dist/css/style.css";
 import "./rich-text-editor-style.css";
 
 interface IProps {
-  disabled?: boolean;
   height?: number | string;
-  placeholder?: string;
+  toolbarConfig?: Partial<IToolbarConfig>;
+  editorConfig?: Partial<IEditorConfig>;
   value?: string;
   onChange?(value: string): void;
 }
@@ -19,13 +21,55 @@ const RapidEditor = memo<IProps>((props) => {
   const [editor, setEditor] = useState<IDomEditor | null>(null);
 
   // 工具栏配置
-  const toolbarConfig: Partial<IToolbarConfig> = {};
+  const toolbarConfig: Partial<IToolbarConfig> = useMemo(
+    () =>
+      merge(
+        {
+          excludeKeys: ["insertVideo", "uploadVideo", "editVideoSize", "group-video"],
+        },
+        props.toolbarConfig || {},
+      ),
+    [props.toolbarConfig],
+  );
 
   // 编辑器配置
-  const editorConfig: Partial<IEditorConfig> = {
-    placeholder: props.placeholder,
-    readOnly: props.disabled || false,
-  };
+  const editorConfig: Partial<IEditorConfig> = useMemo(
+    () =>
+      merge(
+        {
+          placeholder: "请编辑...",
+          readOnly: false,
+          MENU_CONF: {
+            uploadImage: {
+              customUpload(file, insertFn) {
+                return new Promise((resolve) => {
+                  const formData = new FormData();
+                  formData.append("file", file);
+                  rapidApi
+                    .post("/upload", formData, { headers: { "Content-Type": "multipart/form-data" } })
+                    .then((res) => {
+                      const data = res.data;
+                      if (res.status >= 200 && res.status < 400) {
+                        const url = `/api/download/file?inline=true&fileKey=${data.fileKey}&fileName=${file.name}`;
+                        insertFn(url, file.name, url);
+                        resolve("ok");
+                      } else {
+                        throw new Error(data?.message || res.statusText || "图片上传失败");
+                      }
+                    })
+                    .catch((err) => {
+                      const res = err.response;
+                      throw new Error(res?.data?.message || res?.statusText || "图片上传失败");
+                    });
+                });
+              },
+            },
+          },
+        },
+        props.editorConfig || {},
+      ),
+    [props.editorConfig],
+  );
 
   // 及时销毁 editor ，重要！
   useEffect(() => {
@@ -37,7 +81,7 @@ const RapidEditor = memo<IProps>((props) => {
   }, [editor]);
 
   return (
-    <div className="rui-rich-text-editor" style={{ height }}>
+    <div className="rui-rich-text-editor" style={{ height, zIndex: 100 }}>
       <Toolbar editor={editor} defaultConfig={toolbarConfig} mode="default" className="rui-rich-text-editor--toolbar" />
       <Editor
         defaultConfig={editorConfig}
--- packages/rapid-extension/src/rocks/rich-text-editor/RichTextEditor.tsx
+++ packages/rapid-extension/src/rocks/rich-text-editor/RichTextEditor.tsx
@@ -1,8 +1,9 @@
 import { type Rock } from "@ruiapp/move-style";
-import { lazy, memo, PropsWithChildren, useEffect, useState, Suspense } from "react";
+import { lazy, memo, PropsWithChildren, useEffect, useState, Suspense, useMemo } from "react";
 import { RichTextEditorRockConfig } from "./rich-text-editor-types";
 import { convertToEventHandlers } from "@ruiapp/react-renderer";
 import RichTextEditorMeta from "./RichTextEditorMeta";
+import { Spin } from "antd";
 
 const Editor = lazy(() => import("./Editor"));
 
@@ -20,14 +21,21 @@ export default {
   Renderer(context, props) {
     const eventHandlers = convertToEventHandlers({ context, rockConfig: props }) as any;
 
+    const editorConfig = useMemo(
+      () => ({
+        readOnly: props.disabled,
+        placeholder: props.placeholder,
+      }),
+      [props.disabled, props.placeholder],
+    );
+
     return (
       <ClientOnly>
-        <Suspense fallback={<div></div>}>
+        <Suspense fallback={<Spin />}>
           <Editor
-            value={props.value}
-            disabled={props.disabled}
-            placeholder={props.placeholder}
             height={props.height}
+            editorConfig={editorConfig}
+            value={props.value}
             onChange={(v) => {
               eventHandlers.onChange?.(v);
             }}
