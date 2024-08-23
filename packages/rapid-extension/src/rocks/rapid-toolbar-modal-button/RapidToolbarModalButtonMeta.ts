077852e6895d905e11bbdb2a0892261c1b901ce1
Fossil <imzshh@gmail.com>
2024-08-23 10:57:34 +0800

    增加rapidToolbarModalButton组件

---
+++ packages/rapid-extension/src/rocks/rapid-toolbar-modal-button/RapidToolbarModalButtonMeta.ts
@@ -0,0 +1,9 @@
+import type { RockMeta } from "@ruiapp/move-style";
+
+export default {
+  $type: "rapidToolbarModalButton",
+
+  slots: {},
+
+  propertyPanels: [],
+} as RockMeta;
---
+++ packages/rapid-extension/src/rocks/rapid-toolbar-modal-button/rapid-toolbar-modal-button-types.ts
@@ -0,0 +1,17 @@
+import type { RockChildrenConfig, RockEventHandlerConfig, SimpleRockConfig } from "@ruiapp/move-style";
+import { RapidToolbarButtonConfig } from "../rapid-toolbar-button/rapid-toolbar-button-types";
+
+export interface RapidToolbarModalButtonConfig extends Omit<RapidToolbarButtonConfig, "actionEventName"> {
+  /**
+   * 模态框的标题
+   */
+  modalTitle: string;
+
+  modalBody: RockChildrenConfig;
+
+  onModalOk: RockEventHandlerConfig;
+
+  onModalCancel: RockEventHandlerConfig;
+}
+
+export interface RapidToolbarModalButtonRockConfig extends SimpleRockConfig, RapidToolbarModalButtonConfig {}
